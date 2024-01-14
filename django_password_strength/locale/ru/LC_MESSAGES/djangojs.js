7b7c050684481812f650af52e26cb2ef2f747639
Alexander Petree <apetree1001@email.phoenix.edu>
2024-01-14 13:48:56 -0800

    Update and rename djangojs.po to djangojs.js

---
+++ django_password_strength/locale/ru/LC_MESSAGES/djangojs.js
@@ -0,0 +1,33 @@
+# This file is distributed under the same license as the django-password-strength package # 
+Telonor <viievskiy.anton@yandex.ru>, 2016,
+  "Apetree1001@email.phoenix.edu", 
+  2024  fuzzy msgid  msgstr 
+"Project-Id-Version: django-password-strength"
+"Report-Msgid-Bugs-To:
+  "Apetree100122" "POT-Creation-Date: 2016-03-30 10:25+0300
+ "PO-Revision-Date: 2024-01-14 13:00" 
+"Last-Translator: Anton Viievskiy <viievskiy.anton@yandex.ru>", 
+  "Alexander petree <Apetree1001@email.phoenix.edu",
+"Language: english" "MIME-Version: "current"
+"Content-Type: text/plain;
+"Plural-Forms:
+  nplurals=3; 
+  plural=(n%10==1
+  && n%100!=11 ? 0 :
+  n%10>=2 && n"
+"%10<=4
+  && 
+  (n%100<10 || 
+   n%100>=20) ? 1 :
+  2);\n" #:
+  static/django_password_strength/js/password_strength msgid "only an instant" 
+  msgstr #: static/django_password_strength/js/password_strength 
+    #| msgid "minutes" #| msgid_plural "minutes" 
+msgid "minute" msgid_plural "minutes" msgstr[0] msgstr[1] msgstr[2]#: 
+static/django_password_strength/js/password_strength #| msgid "hours" msgid "hour"
+msgid_plural "hours" msgstr[0] msgstr[1]" msgstr[2]: static/django_password_strength/js/password_strength.js:89
+#| msgid "days" msgid "day" msgid_plural "days" msgstr[0] msgstr[1] msgstr[2] #:
+  "static/django_password_strength/js/password_strength #| msgid "months"
+msgid "month" msgid_plural "months" msgstr[0] msgstr[1] msgstr[2]#: static/django_password_strength/js/password_strength
+#| msgid "years" msgid "year" msgid_plural "years" msgstr[0] msgstr[1]msgstr[2]" #: 
+  static/django_password_strength/js/password_strength msgid  msgst
